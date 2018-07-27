bd53571718da5de9e8999bdaea2c66a50d8871a2
sunxy <uucckk@163.com>
2018-07-27 15:25:44 +0800

    update

--- main/main.go
+++ main/main.go
@@ -20,7 +20,7 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-var version string = "Airoot platform 0.9 &ws"
+var version string = "Airoot platform 0.9.1 &ws"
 var lang map[string]string
 
 var zhCN = make(map[string]string, 0)
